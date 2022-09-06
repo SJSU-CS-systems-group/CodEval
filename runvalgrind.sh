fcf93578b53a81c6f60a8897aeaf26a2ea9099c5
ben reed <ben.reed@sjsu.edu>
2022-09-05 20:33:05 -0700

    cleanup runvalgrind

--- runvalgrind.sh
+++ runvalgrind.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-valgrind --log-file="valgrindout" $@
-retval=$?
-script_output $(grep definitely valgrindout)
-exit $retval
+valgrind --leak-check=full --error-exitcode=99 --log-file=val.err ${@}
+rc=$?
+if [ $rc -eq 99 ]
+then
+    script_output 'FAIL!!!! YOU HAVE A MEMORY PROBLEM'
+    cat val.err
+fi
+rm -f val.err
+exit $rc
