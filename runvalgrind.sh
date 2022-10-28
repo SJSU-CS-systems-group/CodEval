37b2f08926bba32985443f00f8e1dd2f9d2b9eec
Aditi <aditi.agrawal@sjsu.edu>
2022-10-28 15:39:23 -0700

    diff and valgrind changes

--- runvalgrind.sh
+++ runvalgrind.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-valgrind --leak-check=full --error-exitcode=99 --log-file=val.err ${@}
+valgrind --xml=yes --xml-file=val.err --leak-check=full --error-exitcode=99 ${@}
 rc=$?
 if [ $rc -eq 99 ]
 then
     script_output 'FAIL!!!! YOU HAVE A MEMORY PROBLEM'
-    cat val.err
+    ./parsevalgrind val.err > ./evaluationLogs/log_of_valgrind
 fi
 rm -f val.err
 exit $rc
