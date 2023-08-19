5bf5f6f77c331a33bfcddd151365d33d9396a5fd
bcr33d <bcr33d@gmail.com>
2023-08-19 11:23:49 -0700

    fix convertMD2Html to handle HT properly

--- convertMD2Html.py
+++ convertMD2Html.py
@@ -40,7 +40,9 @@ def mdToHtml(file_name,file_dict):
                 assignment = text
             elif line.startswith('T ',0,2) or line.startswith('I ',0,2) or line.startswith('O ',0,2) or line.startswith('X ',0,2) or line.startswith('E ',0,2):
                 examples.append(line)
-            elif line.startswith('HT ',0,2):
+            elif line.startswith('HT ',0,3):
+                samples = sampleTestCases(examples,numOfSampleTC)
+                assignment = re.sub('EXMPLS [0-9]+',samples,assignment)
                 break
             else:
                 if 'URL_OF_HW ' in line:
