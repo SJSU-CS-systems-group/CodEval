f33e29c20e59c6955bbfc62878e8cea469244883
ben reed <bcr33d@gmail.com>
2023-10-05 07:33:46 -0700

    clean up valgrind parsing

--- runvalgrind.sh
+++ runvalgrind.sh
@@ -5,6 +5,7 @@ rc=$?
 if [ $rc -eq 99 ]
 then
     script_output 'FAIL!!!! YOU HAVE A MEMORY PROBLEM'
+    mkdir -p evaluationLogs
     ./parsevalgrind val.err > ./evaluationLogs/log_of_valgrind
 fi
 rm -f val.err
