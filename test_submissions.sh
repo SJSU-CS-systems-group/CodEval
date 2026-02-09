4c91ac28e61d2286c78b92932afe212f0da9f69d
ben reed <ben.reed@sjsu.edu>
2026-02-09 14:13:34 -0800

    updated test_submissions.sh to use --active

--- test_submissions.sh
+++ test_submissions.sh
@@ -14,22 +14,22 @@ then
     exit 2
 fi
 
-if [ $# -ne 2 ]
+if [ $# -ne 1 ]
 then
-    echo "USAGE: $0 COURSE ASSIGNMENT"
+    echo "USAGE: $0 COURSE"
     exit 1
 fi
 
 COURSE="$1"
-ASSIGNMENT="$2"
 
 trap 'rm -rf submissions' EXIT
+rm -rf submissions
 
 setfacl -d -m u:$(whoami):rwX .
 ulimit -f 1000000
-assignment-codeval download-submissions "$COURSE" "$ASSIGNMENT" --uncommented_for 20
-assignment-codeval github-setup-repo "$COURSE" "$ASSIGNMENT"
-assignment-codeval evaluate-submissions ~/codeval
+assignment-codeval download-submissions "$COURSE" --active --uncommented_for 20
+assignment-codeval github-setup-repo
+assignment-codeval evaluate-submissions
 assignment-codeval upload-submission-comments submissions
 
 if [[ -n $WATCHDOG_CMD ]]
